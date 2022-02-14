72a521eb6624aeeded035bf60b6f80bab0519749
Shimon-Levinshtein <61378877+Shimon-Levinshtein@users.noreply.github.com>
2022-02-15 01:14:20 +0200

    added icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import GuestRoom from "./components/GuestRoom/GuestRoom";
 import QueueRestaurant from "./components/QueueRestaurant/QueueRestaurant";
 import Host from "./components/Host/Host";
 import CompletedOrders from "./components/CompletedOrders/CompletedOrders";
+import ArtboardA from "./images/svg/ArtboardA.svg";
+import ArtboardB from "./images/svg/ArtboardB.svg";
+import ArtboardC from "./images/svg/ArtboardC.svg";
+import ArtboardD from "./images/svg/ArtboardD.svg";
 
 
 function App() {
@@ -13,6 +17,12 @@ function App() {
         <div className={styles.titleHeader}>
           <h1>Welcome</h1>
           <h2>To the restaurant</h2>
+        </div>
+        <div className={styles.iconsContiner}>
+          <img src={ArtboardA}/>
+          <img src={ArtboardB}/>
+          <img src={ArtboardC}/>
+          <img src={ArtboardD}/>
         </div>
       </div>
       <div className={styles.body}>
